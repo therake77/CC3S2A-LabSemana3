931b7097d517de6824291e56c93281d58250df45
therake77 <jarema9125@hotmail.com>
2026-04-17 15:49:51 -0500

    bug: fixed '/src/routes/todo.routes' not found

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ moongose.connect(process.env.MONGODB_URL)
     .then(() => console.log('Conexión exitosa a MongoDB Atlas'))
     .catch(err => console.error('Error al conectar a BD:', err))
 
-fastify.register(require('/src/routes/todo.routes'), {prefix : '/api/todos'})
+fastify.register(require('./src/routes/todo.routes'), {prefix : '/api/todos'})
 
 const start = async () => {
     try {await fastify.listen({port: process.env.PORT || 3000, host: '0.0.0.0'})}
